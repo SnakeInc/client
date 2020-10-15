55e75b667e6c9435b4bd01d3d9ee1acad5ad3637
Sebastian Diers <admin@yellowphoenix18.de>
2020-10-15 12:23:56 +0200

    feat: websocket

---
+++ src/main/java/de/uol/snakeinc/connection/ConnectionThread.java
@@ -0,0 +1,48 @@
+package de.uol.snakeinc.connection;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Log4j2
+public class ConnectionThread extends Thread {
+
+    private SpeedWebSocketClient webSocket;
+    private URI url;
+    private boolean running;
+    private boolean callback;
+
+    public ConnectionThread(String apiKey) {
+        this.running = true;
+        try {
+            url = new URI("wss://msoll.de/spe_ed?key=" + apiKey);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            callback = false;
+            webSocket = new SpeedWebSocketClient(this, url);
+            webSocket.connect();
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            while (webSocket.isOpen()) {
+            }
+        }
+    }
+
+    public SpeedWebSocketClient getWebSocket() {
+        return this.webSocket;
+    }
+
+    public void callBack() {
+        this.callback = true;
+    }
+}
