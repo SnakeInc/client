d89a6c6cfa6865d4aa1569f33644f1c94d304ce8
Joost <joost.vanmark@ewetel.net>
2020-10-15 13:13:09 +0200

    Uncommitted changes before Merge at 15.10.2020 12:49 [Default Changelist]

--- src/main/java/de/uol/snakeinc/entities/Direction.java
+++ src/main/java/de/uol/snakeinc/entities/Direction.java
@@ -1,9 +1,45 @@
 package de.uol.snakeinc.entities;
 
+import com.puppycrawl.tools.checkstyle.checks.blocks.LeftCurlyCheck;
+
 public enum Direction {
 
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT,
+    INVALID;
+
+
+    public Direction change(PlayerMove move) {
+        switch (move) {
+            case TURN_RIGHT:
+                switch (this) {
+                    case UP:
+                        return RIGHT;
+                    case DOWN:
+                        return LEFT;
+                    case LEFT:
+                        return DOWN;
+                    case RIGHT:
+                        return UP;
+                }
+                break;
+            case TURN_LEFT:
+                switch (this) {
+                    case UP:
+                        return LEFT;
+                    case DOWN:
+                        return RIGHT;
+                    case LEFT:
+                        return UP;
+                    case RIGHT:
+                        return DOWN;
+                }
+                break;
+            default:
+                return INVALID;
+        }
+        return INVALID;
+    }
 }
