5808f173d622ec853e11d54cdda37c9c5c82e039
Joost <joost.vanmark@ewetel.net>
2020-11-12 17:15:26 +0100

    funktioniert?

--- src/main/java/de/uol/snakeinc/possibleMoves/possibleMovesCalculation2.java
+++ src/main/java/de/uol/snakeinc/possibleMoves/possibleMovesCalculation2.java
@@ -10,56 +10,6 @@ import lombok.ToString;
 @CustomLog
 public class possibleMovesCalculation2 {
 
-    private static Stats3 linearStats3(Board from) {
-        int length = from.getPlayers().length;
-        Stats3 res = new Stats3(length);
-        PlayerMap players = new PlayerMap(from.getPlayers());
-        PlayerMap players1 = new PlayerMap(length);
-        IntSet dead = IntSet.ofSize(length);
-        int us = from.getUs();
-        int height = from.getHeight();
-        int width = from.getWidth();
-        int turn = from.getTurn();
-
-        CombinationTree combinationTree1 = new CombinationTree();
-        players.addToCombinationTree(combinationTree1, height, width, turn);
-        var combs1 = combinationTree1.getRawCombinations();
-        for (var combination1 : combs1) {
-            MapCoordinateBag map1 = new MapCoordinateBag(from.getMap());
-            map1.addInternalAll(dead, combination1.toIterator(), players1);
-            updateStats(res.deaths1, dead);
-            res.overall1++;
-
-            dead.clear();
-            CombinationTree combinationTree2 = new CombinationTree();
-            players1.addToCombinationTree(combinationTree2, height, width, turn + 1);
-            players1.clear();
-            var combs2 = combinationTree2.getRawCombinations();
-            for (var combination2 : combs2) {
-                MapCoordinateBag map2 = new MapCoordinateBag(map1);
-                map2.addInternalAll(dead, combination2.toIterator(), players1);
-                updateStats(res.deaths2, dead);
-                res.overall2++;
-
-                dead.clear();
-                CombinationTree combinationTree3 = new CombinationTree();
-                players1.addToCombinationTree(combinationTree3, height, width, turn + 2);
-                players1.clear();
-                var combs3 = combinationTree3.getRawCombinations();
-                for (var combination3 : combs3) {
-                    MapCoordinateBag map3 = new MapCoordinateBag(map2);
-                    map3.addInternalAll(dead, combination3.toIterator(), players1);
-                    updateStats(res.deaths3, dead);
-                    res.overall3++;
-
-                    dead.clear();
-                    players1.clear();
-                }
-            }
-        }
-        return res;
-    }
-
     private static Stats3 linearStats3withCulling(Board from) {
         int length = from.getPlayers().length;
         Stats3 res = new Stats3(length);
@@ -111,8 +61,25 @@ public class possibleMovesCalculation2 {
                     updateStats(res.deaths3, dead);
                     res.overall3++;
 
+                    if (dead.contains(us)) {
+                        players1.clear();
+                        dead.clear();
+                        continue;
+                    }
                     dead.clear();
+
+                    CombinationTree combinationTree4 = new CombinationTree();
+                    players1.addToCombinationTree(combinationTree4, height, width, turn + 3);
                     players1.clear();
+                    var combs4 = combinationTree4.getRawCombinations();
+                    for (var combination4 : combs4) {
+                        MapCoordinateBag map4 = map3.addInternalAll(dead, combination4.toIterator(), players1);
+                        updateStats(res.deaths4, dead);
+                        res.overall4++;
+
+                        players1.clear();
+                        dead.clear();
+                    }
                 }
             }
         }
@@ -153,13 +120,18 @@ public class possibleMovesCalculation2 {
         public int[] deaths3;
         public int overall3;
 
+        public int[] deaths4;
+        public int overall4;
+
         public Stats3(int length) {
             deaths1 = new int[length];
             deaths2 = new int[length];
             deaths3 = new int[length];
+            deaths4 = new int[length];
             overall1 = 0;
             overall2 = 0;
             overall3 = 0;
+            overall4 = 0;
         }
     }
 
