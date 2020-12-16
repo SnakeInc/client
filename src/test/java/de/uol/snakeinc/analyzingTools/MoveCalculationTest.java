c18876a51e78bd10538325b1b17225c892c9ba34
Joost <joost.vanmark@ewetel.net>
2020-12-16 16:17:07 +0100

    checkstyle

--- src/test/java/de/uol/snakeinc/analyzingTools/MoveCalculationTest.java
+++ src/test/java/de/uol/snakeinc/analyzingTools/MoveCalculationTest.java
@@ -10,7 +10,9 @@ import net.jqwik.api.constraints.IntRange;
 class MoveCalculationTest {
 
     @Property
-    boolean testPreCalculateSpeed(@ForAll Action act, @ForAll Direction dir, @ForAll @IntRange(min = 1, max = 10) int speed) {
+    boolean testPreCalculateSpeed(@ForAll Action act,
+                                  @ForAll Direction dir,
+                                  @ForAll @IntRange(min = 1, max = 10) int speed) {
         var moveCalc = new MoveCalculation(new Cell[1][0], null, null);
         var res = moveCalc.preCalculate(act, dir, speed);
         var resDir = res.getDirection();
