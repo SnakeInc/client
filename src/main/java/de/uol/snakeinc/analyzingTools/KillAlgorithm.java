968e5fbf4bcf3de0d94d0f0be62c9ceec92e3240
JordiWelp <jordi.welp@uni-oldenburg.de>
2021-01-06 22:26:13 +0100

    refactor

--- src/main/java/de/uol/snakeinc/analyzingTools/KillAlgorithm.java
+++ src/main/java/de/uol/snakeinc/analyzingTools/KillAlgorithm.java
@@ -93,7 +93,7 @@ public abstract class KillAlgorithm {
      * @param width     width
      * @param height    height
      * @param cells     cells
-     * @return          floodVar
+     * @return floodVar
      */
     private static FloodVar flood(FloodVar floodVar , int x, int y, int width, int height, Cell[][] cells) {
         if (x >= 0 && x < width && y >= 0 && y < height && !cells[x][y].isDeadly()
