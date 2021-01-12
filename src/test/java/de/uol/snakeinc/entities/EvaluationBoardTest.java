3a6a7a036c26c0975ed03f691275c52c84f68c5d
JordiWelp <jordi.welp@uni-oldenburg.de>
2021-01-12 16:29:02 +0100

    Test

--- src/test/java/de/uol/snakeinc/entities/EvaluationBoardTest.java
+++ src/test/java/de/uol/snakeinc/entities/EvaluationBoardTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 class EvaluationBoardTest {
@@ -14,7 +15,7 @@ class EvaluationBoardTest {
         Player op1 = new Player(2, 2, 2, Direction.DOWN, 1,true, "player2");
         Player op2 = new Player(3, 4, 4, Direction.DOWN, 1,true, "player3");
         Player[] players = {us, op1, op2};
-        EvaluationBoard evaluationBoard = new EvaluationBoard(10, 10, players, us, 1);
+        EvaluationBoard evaluationBoard = new EvaluationBoard(10, 10, players, us, 0);
 
         Player usU = new Player(1, 0, 1, Direction.DOWN, 1,true, "player1");
         Player op1U = new Player(2, 2, 3, Direction.DOWN, 1,true, "player2");
@@ -33,30 +34,28 @@ class EvaluationBoardTest {
         Player op1 = new Player(2, 2, 2, Direction.DOWN, 1,true, "player2");
         Player op2 = new Player(3, 4, 4, Direction.DOWN, 1,true, "player3");
         Player[] players = {us, op1, op2};
-        EvaluationBoard evaluationBoard = new EvaluationBoard(10, 10, players, us, 1);
+        EvaluationBoard evaluationBoard = new EvaluationBoard(10, 10, players, us, 0);
 
         Player usU = new Player(1, 0, 1, Direction.DOWN, 1,true, "player1");
         Player op1U = new Player(2, 2, 3, Direction.DOWN, 1,true, "player2");
-        Player op2U = new Player(3, 4, 8, Direction.DOWN, 4,true, "player3");
+        Player op2U = new Player(3, 4, 10, Direction.DOWN, 1,true, "player3");
         HashMap<Integer, Player> playerHashMap = new HashMap<Integer, Player>(Map.of(1, usU, 2, op1U, 3, op2U));
 
         assertEquals(1, evaluationBoard.getCells()[0][1].getRisks());
         evaluationBoard.updatePlayersCells(playerHashMap);
         assertEquals(10, evaluationBoard.getCells()[0][1].getRisks());
-        assertEquals(1, evaluationBoard.getCells()[4][9].getRisks());
-        assertEquals(10, evaluationBoard.getCells()[4][8].getRisks());
-        assertEquals(10, evaluationBoard.getCells()[4][7].getRisks());
-        assertEquals(10, evaluationBoard.getCells()[4][5].getRisks());
-
+        assertEquals(10, evaluationBoard.getCells()[4][9]);
+        assertEquals(10, evaluationBoard.getCells()[4][8]);
+        assertEquals(10, evaluationBoard.getCells()[4][2]);
 
         EvaluationBoard evaluationBoard2 = new EvaluationBoard(10, 10, players, us, 6);
-        assertEquals(1, evaluationBoard2.getCells()[0][1].getRisks());
-        evaluationBoard2.updatePlayersCells(playerHashMap);
-        assertEquals(10, evaluationBoard2.getCells()[0][1].getRisks());
-        assertEquals(10, evaluationBoard2.getCells()[4][8].getRisks());
-        assertEquals(1, evaluationBoard2.getCells()[4][7].getRisks());
-        assertEquals(1, evaluationBoard2.getCells()[4][6].getRisks());
-        assertEquals(10, evaluationBoard2.getCells()[4][5].getRisks());
+        assertEquals(1, evaluationBoard.getCells()[0][1].getRisks());
+        evaluationBoard.updatePlayersCells(playerHashMap);
+        assertEquals(10, evaluationBoard.getCells()[0][1].getRisks());
+        assertEquals(10, evaluationBoard.getCells()[4][9]);
+        assertEquals(1, evaluationBoard.getCells()[4][8]);
+        assertEquals(1, evaluationBoard.getCells()[4][3]);
+        assertEquals(10, evaluationBoard.getCells()[4][2]);
 
 
     }
