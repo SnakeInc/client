0e47dd6948d1acddde9544db9b042fc51d9202d4
JordiWelp <jordi.welp@uni-oldenburg.de>
2021-01-17 23:02:25 +0100

    improvement:KillALgorithm

--- src/main/java/de/uol/snakeinc/Config.java
+++ src/main/java/de/uol/snakeinc/Config.java
@@ -17,7 +17,6 @@ public abstract class Config {
     public static final int INITIAL_ATTACK_DISTANCE = 2;
     public static final int INITIAL_FLOOD_TERMINATION_COUNT = 200;
     public static final int ROUNDS_AHEAD_FOR_KILL = 4;
-    public static final double KILL_INCENTIVE_DEAD_END = 0.8D;
     public static final int MAX_PLAYER_CAP = 3;
 
     /********
@@ -54,6 +53,8 @@ public abstract class Config {
     public static final double MOVE_RISK_2 = 1.0625D;
     public static final double MOVE_RISK_3 = 1.015625D;
     public static final double KILL_INCENTIVE = 0.3D;
+    public static final double KILL_INCENTIVE_DEAD_END = 0.8D;
+
 
 
     /********
