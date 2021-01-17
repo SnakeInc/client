0325769a43d9879f5708bb462ed19528dc07d4c1
Joost <joost.vanmark@ewetel.net>
2021-01-17 11:28:30 +0100

    updated checkstyles

--- src/main/java/de/uol/snakeinc/connection/ConnectionThread.java
+++ src/main/java/de/uol/snakeinc/connection/ConnectionThread.java
@@ -35,7 +35,7 @@ public class ConnectionThread extends Thread {
         try {
             //wss://msoll.de/spe_ed?key=
             //url = new URI("wss://msoll.de/spe_ed?key=" + apiKey);
-            url = new URI("wss://yellowphoenix18.de:554/SnakeInc");
+            url = new URI("wss://yellowphoenix18.de:555/Joost");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
